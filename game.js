848c5acb41926c6a8af850f0917a3d50a753046d
Regan Chan <regan@irythia.com>
2011-02-13 19:27:10 -0500

    Added functionality to preload screen, updated engine.

--- game.js
+++ game.js
@@ -25,7 +25,15 @@ var MS = {
     init : function() {
         DE.InputManager.Keyboard.init();
         DE.InputManager.Mouse.init();
+        DE.InputManager.Touch.init();
         
+        // Dynamically change resolution of the viewport
+        $('#viewport').css({
+            'width' : MS.Config.Resolution.WIDTH,
+            'height' : MS.Config.Resolution.HEIGHT
+        });
+        
+        // First state
         DE.StateManager.push(MS.State.Preload);
         
         setInterval(MS.run, 1000 / MS.Config.REFRESHRATE);
@@ -43,26 +51,132 @@ var MS = {
             loops += 1;
         }
         
-        DE.StateManager.draw();
+        // Draw only when there is an update
+        if (loops) {
+            DE.StateManager.draw();
+        }
     }
 };
+
+/**************************************/
+
+// Game assets (images, level info, etc)
+MS.Assets = {
+    // JavaScript doesn't have a way to import assets automatically, so we
+    // have to specify the locations directly
+    ImageURLs : {
+        background : 'bg.jpg',
+        vine : 'vine.jpg',
+        ice : 'ice.jpg'
+    },
+    // Actual image objects that is used by the game logic
+    Images : {}
+};
+
+/**************************************/
 
 MS.Config = {
     FPS : 30,            // Number of times per second that the game updates
     MAXFRAMESKIP : 5,    // Maximum amount of draw operations that can be skipped
-    REFRESHRATE : 60     // Number of times per second that the game loop runs
+    REFRESHRATE : 60,    // Number of times per second that the game loop runs
+    Resolution : {
+        WIDTH : 480,
+        HEIGHT : 320,
+        ASPECTRATIO : 1.5
+    }
 };
 
-// States of the game - each state has an update and draw operation associated
-// with it
+/**************************************/
+
+// States of the game - each state has the following functions common to them:
+// - start() : called when state is initialized
+// - exit() : called when state is exiting (popped off the stack)
+// - draw() : called when the game loop attempts to render graphics
+// - update() : called when the game loop attempts to update game state
 MS.State = {
     Preload : {
-        update : function() {
-
+        count : 0,         // Number of physical assets loaded
+        totalCount : 0,    // Total number of assets
+        percentage : 0,    // Percentage of assets loaded
+        test : 0,
+        
+        start : function() {
+            DE.Util.log('PRELOAD: Starting state');
+            
+            // Loading screen
+            DE.Renderer.Object.create('title');
+            DE.Renderer.Object.create('loading');
+            
+            DE.InputManager.changeContext(this);
+        },
+        
+        exit : function() {
+            DE.Util.log('PRELOAD: Exiting state');
+            
+            DE.Renderer.Object.destroy('title');
+            DE.Renderer.Object.destroy('loading');
+            
+            DE.InputManager.unsubscribeAll(this.next);
         },
         
         draw : function() {
-
+            $('#title').html('Monkey Spin');
+            $('#loading').html('Loading: ' + this.percentage + '% Completed');
+        },
+        
+        update : function() {
+            // If count is not set, we loop through the image object to...well, count
+            if (!this.totalCount) {
+                for (img in MS.Assets.ImageURLs) {
+                    this.totalCount += 1;
+                }
+                
+                // First preload
+                MS.Assets.Images = DE.Util.preloadImages(MS.Assets.ImageURLs, this.imageLoaded, this);
+            }
+            
+            if (this.count == this.totalCount && typeof(this.update.subscribed) == 'undefined') {
+                this.update.subscribed = true;
+                DE.InputManager.subscribeAll(this.next);
+            }
+        },
+        
+        // void imageLoaded()
+        // Callback function that triggers when images have finished loading
+        imageLoaded : function() {
+            this.count += 1;
+            this.percentage = (this.count / this.totalCount * 100).toFixed(0);
+            DE.Util.log('PRELOAD: Percentage of images loaded: ' + this.percentage + '%');
+        },
+        
+        // void next(obj event, string type)
+        // Processes input to proceed to the next state
+        next : function(event, type) {
+            DE.Util.log('PRELOAD: Event (' + type + ') triggered');
+            
+            // Don't really care about the event - just continue on input
+            DE.StateManager.pop();
+            DE.StateManager.push(MS.State.Main);
+        }
+    },
+    
+    Main : {
+        start : function() {
+            DE.Util.log('MAIN : Starting state');
+            
+            DE.InputManager.changeContext(this);
+        },
+        
+        exit : function() {
+            DE.Util.log('MAIN : Exiting state');
+        },
+        
+        draw : function() {
+            
+        },
+        
+        update : function() {
+            
         }
     }
 };
@@ -81,10 +195,32 @@ DE.Config = {
 
 /**************************************/
 
-DE.InputManager = {};
+DE.InputManager = {
+    // Subscribe to all events
+    subscribeAll : function(fn) {
+        DE.InputManager.Keyboard.subscribe(fn);
+        DE.InputManager.Mouse.subscribe(fn);
+        DE.InputManager.Touch.subscribe(fn);
+    },
+    
+    // Unsubscribe to all events
+    unsubscribeAll : function(fn) {
+        DE.InputManager.Keyboard.unsubscribe(fn);
+        DE.InputManager.Mouse.unsubscribe(fn);
+        DE.InputManager.Touch.unsubscribe(fn);
+    },
+    
+    // Change context
+    changeContext : function(context) {
+        DE.InputManager.Keyboard.context = context;
+        DE.InputManager.Mouse.context = context;
+        DE.InputManager.Touch.context = context;
+    }
+};
 
 DE.InputManager.Base = {
-    functions : [],    // Subscribed functions
+    functions : [],      // Subscribed functions
+    context : window,    // Rebinds "this"
     
     // void subscribe(function fn)
     // Adds a function to the array that will listen to triggered events
@@ -100,55 +236,81 @@ DE.InputManager.Base = {
         });
     },
     
-    // void trigger(obj event, obj context)
+    // void trigger(obj event, string type, [obj context])
     // Calls function with "this" set to context and event (keypress, click,
     // etc.) passed as an argument
-    trigger : function(event, context) {
-        var scope = context || window;
+    trigger : function(event, type, context) {
+        var _context = context || this.context;
         this.functions.forEach(function(elt) {
-            elt.call(scope, event);
+            elt.call(_context, event, type);
         });
     }
 };
 
-DE.InputManager.Keyboard = Object.create(DE.InputManager.Base);
+DE.InputManager.Keyboard = $.extend(true, {}, DE.InputManager.Base);
 DE.InputManager.Keyboard.init = function() {
     var that = this;
     $(document).keypress(function(event) {
-        that.trigger(event);
+        that.trigger(event, 'keypress');
     });
 };
 
-DE.InputManager.Mouse = Object.create(DE.InputManager.Base);
+DE.InputManager.Mouse = $.extend(true, {}, DE.InputManager.Base);
 DE.InputManager.Mouse.init = function() {
     var that = this;
     $(document).click(function(event) {
-        that.trigger(event);
-    })
+        that.trigger(event, 'click');
+    });
 };
+
+DE.InputManager.Touch = $.extend(true, {}, DE.InputManager.Base);
+DE.InputManager.Touch.init = function() {
+    var that = this;
+    $(document).bind('tap', function(event) {
+        that.trigger(event, 'tap');
+    });
+}
 
 /**************************************/
 
 DE.Renderer = {};
+DE.Renderer.Object = {
+    // void create(string id, [string x], [string y], [string width], [string height])
+    // Create element on rendering surface
+    create : function(id, x, y, width, height) {
+        var _x = x || 'auto';
+        var _y = y || 'auto';
+        var _width = width || 'auto';
+        var _height = height || 'auto';
+        $html = $('<div id="' + id + '"></div>').css({
+            width : _width,
+            height : _height,
+            left : _x,
+            top : _y
+        });
+        $('#viewport').append($html);
+    },
+    
+    // void destroy(string id)
+    // Delete element from rendering surface
+    destroy : function(id) {
+        $('#' + id, '#viewport').remove();
+    },
+    
+    // void translate(string id, string x, string y)
+    // Move object to a new position
+    translate : function(id, x, y) {
+        $('#' + id).css({
+            'left' : x,
+            'top' : y
+        })
+    }
+};
 
 /**************************************/
 
 DE.StateManager = {
     states : [],    // State stack
-    
-    // void add(obj state)
-    // Add state to the end of the stack
-    push : function(state) {
-        this.states.push(state);
-    },
-    
-    // void remove()
-    // Remove state from the end of the stack
-    pop : function() {
-        if (this.states.length > 0) {
-            this.states.pop();
-        }
-    },
     
     // mixed getCurState()
     // Gets current state from the stack. Returns the state or null if stack
@@ -160,21 +322,41 @@ DE.StateManager = {
         return null;
     },
     
-    // void update()
-    // Updates the current state
-    update : function() {
-        var state = this.getCurState();
-        if (state && typeof(state.update) == 'function') {
-            state.update();
+    // void remove()
+    // Remove state from the end of the stack
+    pop : function() {
+        if (this.states.length > 0) {
+            var state = this.states.pop();
+            if (typeof(state.exit) == 'function') {
+                state.exit();
+            }
         }
     },
     
+    // void add(obj state)
+    // Add state to the end of the stack
+    push : function(state) {
+        this.states.push(state);
+        if (typeof(state.start) == 'function') {
+            state.start();
+        }
+    },
+
     // void draw()
     // Draws the current state
     draw : function() {
         var state = this.getCurState();
         if (state && typeof(state.draw) == 'function'){
             state.draw();
+        }
+    },
+    
+    // void update()
+    // Updates the current state
+    update : function() {
+        var state = this.getCurState();
+        if (state && typeof(state.update) == 'function') {
+            state.update();
         }
     }
 };
@@ -192,19 +374,20 @@ DE.Util = {
         }
     },
     
-    // object preloadImages(object images, function callback)
+    // object preloadImages(object images, function callback, [obj context])
     // Loads images into the browser cache to prevent flickering and other
     // rendering "bugs" during execution. Returns when everything has
     // finished loading. The callback function executes once for every image
     // after it has fully loaded (i.e. is in cache).
-    preloadImages : function(images, callback) {
-        result = {};
+    preloadImages : function(images, callback, context) {
+        var result = {};
+        var scope = context || window;
         for (elt in images) {
             result[elt] = new Image();
             result[elt].src = images[elt];
             $(result[elt]).load(function() {
                 if (typeof callback == 'function') {
-                    callback();
+                    callback.call(scope);
                 }
             });
         }
