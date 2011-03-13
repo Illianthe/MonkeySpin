c79c5b6c9af2dfc0ab9837868041415525404f0f
Regan Chan <regan@irythia.com>
2011-03-12 20:09:25 -0500

    Change file extension.

---
+++ game/MS.Game/MS.Game.Actions.js
@@ -0,0 +1,25 @@
+/**
+ * Actions that a player can take in the game.
+ */
+MS.Game.Actions = {
+    /**
+     * void spin()
+     */
+    spin : function() {
+        
+    },
+    
+    /**
+     * void climb()
+     */
+    climb : function() {
+        
+    },
+    
+    /**
+     * void jump()
+     */
+    jump : function() {
+        
+    }
+};
