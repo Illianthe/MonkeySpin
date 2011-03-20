376c093a075fcaa9e3290b7f260ee17b0421d193
Regan Chan <regan@irythia.com>
2011-03-20 04:54:02 -0400

    Debugging on iOS.

--- game/MS.State/MS.State.Main.js
+++ game/MS.State/MS.State.Main.js
@@ -11,15 +11,7 @@ MS.State.Main = {
     },
     
     update : function() {
-        if (typeof(this.update.subscribed) == 'undefined') {
-            this.update.subscribed = true;
-            
-            var next = function(event, type) {
-                DE.InputManager.unsubscribeAll(next);
-                DE.StateManager.pop();
-                DE.StateManager.push(MS.State.Game);
-            }
-            DE.InputManager.subscribeAll(next);
-        }
+        DE.StateManager.pop();
+        DE.StateManager.push(MS.State.Game);
     }
 };
