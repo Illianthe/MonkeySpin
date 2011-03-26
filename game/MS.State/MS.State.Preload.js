ec468bd130687f09f746ad244ec5e8b1b7641bc8
Regan Chan <regan@irythia.com>
2011-03-26 00:11:12 -0400

    And let's keep preload stall for now.

--- game/MS.State/MS.State.Preload.js
+++ game/MS.State/MS.State.Preload.js
@@ -32,8 +32,23 @@ MS.State.Preload = {
             // Minor change for when there are no assets to be loaded
             this.percentage = this.percentage || 100; 
             
-            DE.StateManager.pop();
-            DE.StateManager.push(MS.State.Main);
+            // Stall on loading screen in debugging mode
+            if (DE.Config.DEBUG) {
+                var that = this;
+                var next = function(event, type) {
+                    DE.Util.log('PRELOAD: Event (' + type + ') triggered');
+            
+                    // Don't really care about the event - just continue on input
+                    DE.InputManager.unsubscribeAll(next);
+                    DE.StateManager.pop();
+                    DE.StateManager.push(MS.State.Main);
+                }
+                DE.InputManager.subscribeAll(next);
+            }
+            else {
+                DE.StateManager.pop();
+                DE.StateManager.push(MS.State.Main);
+            }
         }
     },
     
